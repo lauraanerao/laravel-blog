50a725d4acc5a850056863b82997a99b71dbad5a
Laura <lauralins@gmail.com>
2022-12-17 21:58:35 -0500

    Primeiro commit.

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//Categoria
+    //Controller
+    //Model
+    //Migration
+        //Name
+        //description
+        //slug
+
+//Posts
+    //Controller
+    //Model
+    //Migration
+        //id do usuário
+        //title
+        //description
+        //id category
+        //nome da imagem
+        //slug
+
+
